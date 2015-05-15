66d0cb811d63908fc3f6b63c086148a61e9cfe40
Myrdhinn <sebastien.famel@gmail.com>
2015-05-15 16:25:32 +0200

    Simplify optionName in cobalt.conf

--- HelloWorld/src/main/java/com/cobaltians/helloworld/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/cobaltians/helloworld/HelloWorldApplication.java
@@ -13,5 +13,6 @@ public class HelloWorldApplication extends Application {
         super.onCreate();
 
         Cobalt.getInstance(this).setResourcePath("www/common/");
+        Cobalt.getInstance(this).setPackageName(getPackageName() + ".activities.");
     }
 }

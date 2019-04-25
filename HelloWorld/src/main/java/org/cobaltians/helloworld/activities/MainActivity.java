d01d834f44178fb69242cc9e9287bb2794aa3d82
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-25 14:55:01 +0200

    upgraded to cobalt1.0

--- HelloWorld/src/main/java/org/cobaltians/helloworld/activities/MainActivity.java
+++ HelloWorld/src/main/java/org/cobaltians/helloworld/activities/MainActivity.java
@@ -1,14 +1,7 @@
 package org.cobaltians.helloworld.activities;
 
-import org.cobaltians.helloworld.fragments.MainFragment;
-
-import org.cobaltians.cobalt.Cobalt;
 import org.cobaltians.cobalt.activities.CobaltActivity;
-import org.cobaltians.cobalt.fragments.CobaltFragment;
 
 public class MainActivity extends CobaltActivity {
-
-	protected CobaltFragment getFragment() {
-		return Cobalt.getInstance(this).getFragmentForController(MainFragment.class, "default", "index.html");
-	}
+	
 }
